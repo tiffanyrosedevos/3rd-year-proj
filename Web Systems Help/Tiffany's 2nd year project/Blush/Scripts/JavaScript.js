0edeaced823261689744ff692d46b3d926abfac7
Tiffany <tiffanyrosedevos@gmail.com>
2019-08-06 11:45:06 +0200

    Added web systems files from last year to help you

---
+++ Web Systems Help/Tiffany's 2nd year project/Blush/Scripts/JavaScript.js
@@ -0,0 +1,13 @@
+﻿$("#myLink").click(function (e) {
+
+    e.preventDefault();
+    $.ajax({
+
+        url: $(this).attr("href"), // comma here instead of semicolon   
+        success: function () {
+            alert("Added to Cart");  // or any other indication if you want to show
+        }
+
+    });
+
+});
