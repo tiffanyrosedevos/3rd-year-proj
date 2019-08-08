e5d77f468ba6bb55982434cef71aa43f22170df0
Tiffany <tiffanyrosedevos@gmail.com>
2019-08-08 13:31:37 +0200

    Got the process of connecting to the database figured out

---
+++ BucketList/BucketList/Models/UserFriend.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BucketList.Models
+{
+    public class UserFriend
+    {
+        public int userID { get; set; }
+        public int friendID { get; set; }
+        public string firstName { get; set; }
+        public string surname { get; set; }
+        public int points { get; set; }
+
+        public List<String> achievements { get; set; } // just string of the name
+    }
+}
---
+++ BucketList/BucketList/Views/Home/FriendController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BucketList.Views.Home
+{
+    public class FriendController : Controller
+    {
+        [HttpGet]
+        public ActionResult getFriendProfile(int userID, int friendID)
+        {
+
+
+            try
+            {
+                string CS = ConfigurationManager.ConnectionStrings["DatabaseEntities1"].ConnectionString;
+                SqlConnection conn = new SqlConnection(CS);
+                conn.Open();
+                SqlCommand command = new SqlCommand("Select * from Challenge", conn);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+
+                }
+                conn.Close();
+                reader.Close();
+
+            }
+            catch
+            {
+
+            }
+
+            return View();
+        }
+    }
+}
